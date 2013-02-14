31ffb3d1f8711c65129ebfcf15f4607e8a48749b
Luís Lopes <luismiguellopes@gmail.com>
2013-02-14 11:52:41 +0000

    add project

---
+++ LLopes.SortWithLinq/LLopes.SortWithLinq.Test/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace LLopes.SortWithLinq.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static List<int> BuildUnsortedList(int size)
+        {
+            var rnd = new Random();
+            var list = new List<int>();
+            for (int i = 0; i < size; ++i)
+            {
+                list.Add(rnd.Next(int.MaxValue));
+            }
+            Debug.WriteLine("list generation completed");
+            return list;
+        }
+
+        static bool ListCheck(IEnumerable<int> list)
+        {
+            var n = list.First();
+            foreach (var x in list)
+            {
+                if (n > x)
+                {
+                    Debug.WriteLine("ERROR: List is not sorted");
+                    return false;
+                }
+            }
+            Debug.WriteLine("List is sorted");
+            return true;
+        }
+
+        [TestMethod]
+        public void LinqIntQuickSortTest()
+        {
+            IEnumerable<int> sortedList = null;
+            var unsortedList = BuildUnsortedList(100);
+
+            sortedList = QuickSortSamples.LinqIntQuickSort(unsortedList);
+
+            ListCheck(sortedList);
+        }
+
+        [TestMethod]
+        public void LinqGenericQuickSortTest()
+        {
+            IEnumerable<int> sortedList = null;
+            var unsortedList = BuildUnsortedList(100);
+
+            sortedList = QuickSortSamples.LinqGenericQuickSort(unsortedList, (x, y) => x - y);
+
+            ListCheck(sortedList);
+        }
+
+        [TestMethod]
+        public void TestLambdaGenericQuickSortTest()
+        {
+            IEnumerable<int> sortedList = null;
+            var unsortedList = BuildUnsortedList(100);
+
+            sortedList = QuickSortSamples.LambdaGenericQuickSort(unsortedList);
+
+            ListCheck(sortedList);
+        }
+    }
+}
---
+++ LLopes.SortWithLinq/LLopes.SortWithLinq/QuickSortSamples.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LLopes.SortWithLinq
+{
+    public static class QuickSortSamples
+    {
+        public static IEnumerable<int> LinqIntQuickSort(IEnumerable<int> x)
+        {
+            return x.Count() <= 1
+                ? x
+                : LinqIntQuickSort(x.Skip(1).Where(z => z <= x.First()))
+                    .Concat(new List<int> { x.First() })
+                    .Concat(LinqIntQuickSort(x.Skip(1).Where(z => z > x.First())));
+        }
+
+        public static IEnumerable<T> LinqGenericQuickSort<T>(IEnumerable<T> x, Func<T, T, int> fnCompare)
+        {
+            return x.Count() <= 1
+                ? x
+                : LinqGenericQuickSort(x.Skip(1)
+                        .Where(z => fnCompare(z, x.First()) <= 0), fnCompare)
+                    .Concat(new List<T> { x.First() })
+                    .Concat(LinqGenericQuickSort(x.Skip(1)
+                        .Where(z => fnCompare(z, x.First()) > 0), fnCompare));
+        }
+
+        public static IEnumerable<int> LambdaGenericQuickSort(IEnumerable<int> list)
+        {
+            Func<IEnumerable<int>, Func<int, int, int>, IEnumerable<int>> fn = null;
+            fn = (x1, fnc) => x1.Count() <= 1
+                ? x1
+                : fn(x1.Skip(1).Where(z => fnc(z, x1.First()) <= 0), fnc)
+                    .Concat(new List<int> { x1.First() })
+                    .Concat(fn(x1.Skip(1).Where(z => fnc(z, x1.First()) > 0), fnc));
+
+            return fn(list, (x, y) => x - y);
+        }
+
+
+    }
+}
