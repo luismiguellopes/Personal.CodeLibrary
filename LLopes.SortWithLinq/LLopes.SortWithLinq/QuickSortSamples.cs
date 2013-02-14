a72581140fb9240c30a554ac9219824c4272544d
Luís Lopes <luismiguellopes@gmail.com>
2013-02-14 18:50:38 +0000

    small fixes

--- LLopes.SortWithLinq/LLopes.SortWithLinq/QuickSortSamples.cs
+++ LLopes.SortWithLinq/LLopes.SortWithLinq/QuickSortSamples.cs
@@ -30,11 +30,11 @@ namespace LLopes.SortWithLinq
         public static IEnumerable<int> LambdaGenericQuickSort(IEnumerable<int> list)
         {
             Func<IEnumerable<int>, Func<int, int, int>, IEnumerable<int>> fn = null;
-            fn = (x1, fnc) => x1.Count() <= 1
-                ? x1
-                : fn(x1.Skip(1).Where(z => fnc(z, x1.First()) <= 0), fnc)
-                    .Concat(new List<int> { x1.First() })
-                    .Concat(fn(x1.Skip(1).Where(z => fnc(z, x1.First()) > 0), fnc));
+            fn = (l1, fnCompare) => l1.Count() <= 1
+                ? l1
+                : fn(l1.Skip(1).Where(z => fnCompare(z, l1.First()) <= 0), fnCompare)
+                    .Concat(new List<int> { l1.First() })
+                    .Concat(fn(l1.Skip(1).Where(z => fnCompare(z, l1.First()) > 0), fnCompare));
 
             return fn(list, (x, y) => x - y);
         }
